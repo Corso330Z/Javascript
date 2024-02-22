0738bb6108693b12e37914d232393faabf44801e
Corso330Z <joao.corso@estudante.ifro.edu.br>
2024-02-22 11:23:36 -0400

    atividade 1

--- atividades-22-02/atividade-01.js
+++ atividades-22-02/atividade-01.js
@@ -1,3 +1,3 @@
-var nome = "João"
-var idade = 16
-console.log("Olá, meu nome é", nome,"e eu tenho", idade,"anos.")
+var nome = "João";
+var idade = 16;
+console.log("Olá, meu nome é", nome,"e eu tenho", idade,"anos.");
