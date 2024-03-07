246097ee00e7b8b63a833504864f0fe7033b288e
Corso330Z <joao.corso@estudante.ifro.edu.br>
2024-03-07 09:38:33 -0400

    save

---
+++ aulas.js
@@ -0,0 +1,37 @@
+// BROWSERS são navegadores
+// Fullstack é aquele que funciona para front e back end, o java script é um. Outra definição é de dominar todas as tecnologias que a aplicação usa
+// NODE é um interpretador
+
+
+// ECMASScript - padrão da linguagem Java Script
+
+// Variáveis:
+// Variáveis são usadas para armazenar dados
+// Ex: var nome = "Notebook" ou var preco = 2000
+// Não precisa por tipo, pois Java script é fracamente tipada
+// Var pode ser usado para mudar o valor
+// const - para uma váriavel constante, ou seja, imutável, como o valor de PI
+// let - serve para todo o resto
+// let é a forma preferida atualmente
+
+// Tipos de dados: String, number, boolean.
+
+// .toFixed(2) - arredonda para os dois números após o ponto
+// console.log - tipo o print
+// toLowerCase - Deixa no minúsculo 
+// toString - Converte em string
+// typeof - vê o tipo da variável |ex: console.log(typeof valor)| vai imprimir number
+// Array é uma coleção de dados e com ele a váriavel pode ter mais de um valor, sendo o primeiro elemento representado pelo 0
+// var estados = ["RO", "SP"] - estados[0] = RO
+// No JavaScript podemos armazenar mais de um tipo em um array
+// console.log(array.lenght); - contar a quantidade de varíaveis em uma variável
+// Undefined - Indefinido
+// NaN - Not a number - para evitar isso comece sempre uma varíavel number com 0
+// Null - Nulo |var nome;
+// Se eu for fazer uma conta com o null, seu valor será transformado em 0
+
+// Constante:
+// const aula = "JavaScript"
+// aula = JS
+// Type error - pois não pode ser mudada
+// Se necessário alguma auteração, você precisará mudar onde a const foi declarada
