bb578da74c8e334af28a5e143c20ba74d2543b72
Corso330Z <joao.corso@estudante.ifro.edu.br>
2024-03-07 10:19:49 -0400

    save

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import entradaDados from "readline-sync";
 
-let nome = entradaDados.question("Digite seu nome: ");
-console.log(`Olá, ${nome}.`);
+// let nome = entradaDados.question("Digite seu nome: ");
+// console.log(`Olá, ${nome}.`);
+
+let x = entradaDados.questionInt("X: ") // Tem várias formas de question
+let y = entradaDados.questionInt("Y: ")
+// let total = Number(x) + Number(y);
+console.log(`Total: ${x+y}`)
